498fc3bcdd9a453fad49e5f1e27e04846aa9347f
abbas ebadian <abbasebadiann@gmail.com>
2021-10-04 08:15:31 +0330

    req

--- pcpc/__init__.py
+++ pcpc/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_uploads import configure_uploads, IMAGES, UploadSet
-from flaskext.sass import sass
 import os
 from flask_assets import Environment, Bundle
 
